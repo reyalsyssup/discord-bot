079a7f475c1532757a1a0fdd1ccb4983e51ab61c
Lukas-cag <lukasc7890@gmail.com>
2021-02-22 20:28:46 +1100

    Bot now says hello world in server general channel

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ const Discord = require("discord.js"),
 	bot = new Discord.Client(),
 	{ tok } = require("./config.json");
 
+//Client constant 
+const client = new Discord.Client()
+
 bot.on("ready", () => {
 	console.log("Giddy up!");
+	//Gets the general channel using the channel id
+	var generalChannel = client.channels.get("728075420104130640")
+	//Sends a message to general channel
+	generalChannel.send("Hello, world!")
 });
+
+
 
 bot.login(tok);
