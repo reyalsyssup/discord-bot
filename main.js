fdbd33e422f49b49f00280e22b94e0ae30bd97f7
Kai <m3sv.is.me@gmail.com>
2021-02-23 14:03:00 +1100

    basic help embed

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 const Discord = require("discord.js"),
+	// client
 	bot = new Discord.Client(),
-	{ tok } = require("./config.json");
+	{ tok, prefix } = require("./config.json");
 
-//Client constant 
-const client = new Discord.Client()
-
-bot.on("ready", (msg) => {
+bot.on("ready", () => {
 	console.log("Giddy up!");
 });
 
-client.on('message', (receivedMessage) => {
-	//Prevents bot from replying to itself... because that would be very very bad
-	if (receivedMessage.author == client.user) {
-		return
-	}
+// msg var is the message the bot picked up
+bot.on("message", msg => {
+	if (msg.author !== bot.user) {
+		if (msg.content === "ping") {
+			msg.reply("pong");
+		}
+		// Prefix commands
+		if (msg.content.startsWith(`${prefix}help`)) {
+			let helpEmbed = new Discord.MessageEmbed()
+				.setColor(0xffaa55)
+				.setTitle("Help")
+				.setURL("https://reyalsyssup.github.io/discord-bot")
+				.addFields({ name: "k!ping", value: 'bot replies with "pong!"' });
 
-	//Replys if message is equal to ping
-	if(receivedMessage.content === "ping"){
-		receivedMessage.reply("pong");
+			msg.reply(helpEmbed);
+		}
 	}
 });
-
-
 
 bot.login(tok);
